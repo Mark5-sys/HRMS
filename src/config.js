4fa986e789154ffe89f9a91e8cc7204af95a80ba
TafadzwaG <60160165+TafadzwaG@users.noreply.github.com>
2023-10-10 11:40:27 +0200

    hosted api

--- src/config.js
+++ src/config.js
@@ -1,3 +1,6 @@
-export const API = "http://localhost:8000/api"
+export const API = "https://seal-app-sq4gf.ondigitalocean.app/api"
 
-export const IMAGE_URL = "http://localhost:8000/images"
+export const IMAGE_URL = "https://seal-app-sq4gf.ondigitalocean.app/images"
+
+
+// https://seal-app-sq4gf.ondigitalocean.app/api
